731012c454afa9d81cf9b35514702cbad51fedf2
Krishna <gd14405@gmail.com>
2026-03-28 22:10:04 +0530

    fixed it

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "Agriismart",
     slug: "agriismart",
-    version: "1.1.0",
+    version: "2.0.0",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "agriismart",
